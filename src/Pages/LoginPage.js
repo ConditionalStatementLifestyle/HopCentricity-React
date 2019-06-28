92f4feb89fcb73698393a081ebae795b5ba2bc6e
github username <github email address>
2019-06-28 14:31:41 -0700

    Added loader for login screen, added button for mobile login

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,7 +1,7 @@
 import React  from 'react'
 import Login from '../Components/Login'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-import { Transition, Image } from 'semantic-ui-react'
+import { Transition, Dimmer, Loader, Image } from 'semantic-ui-react'
 import BeerBottle from '../BeerBottle.png'
 import { withRouter } from "react-router-dom";
 import Box from '../Components/DragHop'
@@ -46,11 +46,15 @@ class LoginPage extends React.Component {
     this.setState({Beer: false})
   }
 
+  enterSite = () => {
+    this.props.history.push('/menu')
+    this.props.setSiteEntered(true)
+  }
+
   checkIfHopInBeer = () => {
     if (this.state.inBeer && this.state.inHop) {
       if (localStorage.getItem("HopCentricity_Token") !== null) {
-        this.props.history.push('/menu')
-        this.props.setSiteEntered(true)
+        this.enterSite()
       }
     }
   }
@@ -63,15 +67,29 @@ class LoginPage extends React.Component {
             <h2 className='login-title'>Hop Centricity</h2>
             {
               this.props.user.username === '' ?
-                <Login
-                  setStateUsernameEmailToken={this.props.setStateUsernameEmailToken}
-                  user={this.props.user}
-                />
+                <div>
+                  <Login
+                    setStateUsernameEmailToken={this.props.setStateUsernameEmailToken}
+                    user={this.props.user}
+                    setLoading={this.props.setLoading}
+                  />
+                  <Dimmer active={this.props.isLoading}>
+                    <Loader />
+                  </Dimmer>
+                </div>
               :
                 <ReactCSSTransitionGroup transitionName="pageTransition" transitionEnterTimeout={500} transitionLeaveTimeout={200}>
                   <div className='outer'>\
                     <h3 className='login-message'>
                       Welcome {localStorage.getItem("HopCentricity_Username").split(' ')[0]}, please move the hop into the beer to enter the site
+                    </h3>
+                    <h4 className='login-message'>
+                      For Mobile Devices
+                    </h4>
+                    <h3 className='login-message'>
+                      <button className="ui inverted teal button menuButton huge" onClick={this.enterSite}>
+                        Mobile Login
+                      </button>
                     </h3>
                   </div>
                 </ReactCSSTransitionGroup>
